7ade21eff1b0912793e893b6fc4abb566f468a74
mackawara <mkawara@outlook.com>
2023-06-14 14:43:24 +0200

    added more refined messages && git push origin

--- adverts.js
+++ adverts.js
@@ -1,57 +1,20 @@
 const advertMessages = [
-    `*Introducing the SSD: The Hard Drive's Cooler, Faster, More Efficient Cousin*\n
-    Hard drives are so last year. They're slow, they're loud, and they take up a lot of space. If you're still using a hard drive, you're missing out on all the benefits of SSDs.
-    
-    SSDs are faster than hard drives by a mile. They can boot up your computer in seconds, load applications in a fraction of the time, and make your games run smoother than ever before.
-    
-    SSDs are also quieter than hard drives. They don't have any moving parts, so they don't make any noise when you're using them.
-    
-    And SSDs are more efficient than hard drives. They use less power, so they can help you save money on your energy bill.
-    
-    So if you're looking for a faster, quieter, and more efficient way to store your data, it's time to switch to an SSD.
-    
-    Here are some more reasons why you should switch to an SSD:
-    
-    SSDs are more durable than hard drives. They don't have any moving parts, so they're less likely to break.
-    SSDs are more resistant to shock and vibration. This makes them a good choice for laptops and other portable devices.
-    SSDs are more secure than hard drives. They use encryption to protect your data from unauthorized access.
-    Get yourself an SSD , starting from $35-00 , Call/whatsapp 0775231426. Visit our our shop , \nFirst shop, TM shops ( upstairs from TM)`,
-    "*Toner powder,\nRefill ink,\nToner cartridge reset chip,\nImaging drum,\nMagnetic Drum*\nAll available at Venta Tech, Shop1 TM mall( Shops upstairs from TM), entrance is between between Post office and TM (behind new Banc ABC)",
-    "*$6 32GB $8 64GB* Original Kingston data traveller flash drive \n Call/Whatsapp 0775231426/0812821641",
-    "*Toner cartridges,Ink Cartridges, Master and ink for heavy duty copiers* , \n Venta tech are the premier suppliers of all printer consumables. *Contact us*  for all major brands of printers, HP , Kyocera, Nashua, Lexmark, Canon , Samsung i.e. *Also available* refill toner powder,cartridges, master & ink for copiers are available. Call or whatsapp *0775231426* or visit our shop at TM Pick n Pay Mall ( Shops above TM *Entrance between TM and Post office*) ",
-    "What is your excuse? Still using a Hard Disk Drive (HDD) In 2023??\nAre your files not worth anything to you??\nSSDs are cheaper 3x faster and more durable.Call us and get yours *starting from $35* (including free intallaton of OS and migration of files)",
-    "*Toner powder,\nRefill ink,\nToner cartridge reset chip,\nImaging drum,\nMagnetic Drum*\nAll available at Venta Tech, Shop1 TM mall( Shops upstairs from TM), entrance is between between Post office and TM (behind new Banc ABC)",
-    "*$6 32GB $8 64GB* Original Kingston data traveller flash drive \n Call/Whatsapp 0775231426/0812821641",
-    "*Toner cartridges,Ink Cartridges, Master and ink for heavy duty copiers* , \n Venta tech are the premier suppliers of all printer consumables. *Contact us*  for all major brands of printers, HP , Kyocera, Nashua, Lexmark, Canon , Samsung i.e. *Also available* refill toner powder,cartridges, master & ink for copiers are available. Call or whatsapp *0775231426* or visit our shop at TM Pick n Pay Mall ( Shops above TM *Entrance between TM and Post office*) ",
-    " Original printer consumables available at great prices. Visit Millennium Printers, Shop 1 Baobab Service Station. In stock Hp ,Kyocera,Nashua, Ricoh,Samsung and all other major brands, Call us or whatsapp on 0775 231 426 for enquiries,or visit our shop at Total Baobab Service Station",
-    `*CCTV Cameras and Installation* \n Secure your home or business premises with High quality IP Surveillance Cameras \n
-        *Remote Monitoring* \n monitor live feed from all your cameras from your phone/computer from anywhere in the world \n
-        *Facial Recognition* \n  Our HD surveillance cameras make it easy to identify intruders from more than 10m away \n 
-        *Infra red Light* \n Infrared (IR) cameras provide clear HD pictures in low light or night time \n
-        *Motion Detection* \n get alerts when intruder is detected on the property during non-waking (night time) hours \n
-        Get in touch with Industry legends Millennium printer on whatsapp/call 0775 231 426 `,
-    `*Home or Business Network Engineering* \n
-       2022 Offices/Homes need built-in network wiring. Network wiring is now just as essential as electrical cabling so that all rooms/offices have wired internet(ethernet ports) . \n
-        Millennium Printers, your IT legends, provide custom built home/office network solutions \n 
-        We also offer *WiFI* solutions \n
-        - Wifi range extension\n
-        - Covering WIfi blindspots \n
-        - PtP wireless links \n
-        Contact us on *0775231426* or visit our shop at Total Baobab Service Station,
-        2022 Offices/Homes need built-in network wiring. Network wiring is now just as essential as electrical cabling so that all rooms/offices have wired internet(ethernet ports) . \n
-         Millennium Printers, your IT legends, provide custom built home/office network solutions \n 
-         We also offer *WiFI* solutions \n
-         - Wifi range extension\n
-         - Covering WIfi blindspots \n
-         - PtP wireless links \n
-         Contact us on *0775231426* or visit our shop at Total Baobab Service Station`,
-    `*Computer Repairs, Printer Servicing* \n Reliable,Proffessional Computer hardware and Software Repairs by industry experts. \n Please inbox for enquiries`,
-    "*FASTEST WIFI HOTSPOT IN THE LAND* \n Visit us at \n  VENTA TECH LOCATION: SHOP NO1 Above Pick N Pay Shops (Shops above TM *Entrance between TM and Post*), Hwange   APP or CALL0775231426 Download :\nMovies \n Music \nSoftwares \n   $1 Voucher For 1 hour \n $3 Voucher for 2GB  \n 30 minutes free WiFi for school children in school uniforms",
-    "*CCTV and alarm Systems* brought to you by VENTA TECH \nLOCATION : SHOP NO1  Above TM Pick N Pay Flats, Hwange\n  APP OR CALL 0775231426\n  Professional surveillance (CCTV)  and intrusion detection (ALARM SYSTEMS)\n  HD CAMERAS WITH\nInfrared Vision for low light recording\n  Motion detection\n  Remote viewing ( monitor your cameras from wherever you are)\n Intrusion alerts on your cellphone\n  SECURITY THAT IS  ALWAYS ALERT",
-    "*Serialised Receipt Books In full colour*, Business Documents,VENTA TECH provides proffessionally business documents such as Receipt books,Invoice Books, \n LOCATION SHOP NO. 1 Above TM Pick N Pay Flats Hwange \n App or Call 0775231426 \n For all your \n \n Receipts Books  \nInvoice Books \nLog Books \nReports Books \n 20 pack display books \nA4 Envelopes  \n Rulers \nMathematical sets \nDictionary \nSecondary text books ",
-    "*VSAT Internet*, your internet solution for remote areas.\nCall us on 0775231426 and you can have an internet connetcion within 48 hours,literally anywhere , we have unlimited coverage. Ideal for safaris, mines, rural setup etc.Fast reliable internet from Utande:  *Registered Utande reseller*.",
-    "*Automated WhatsApp chatbot* Run automatic adverts, automatic replies, bulk messaging, automatic alerts\nWebsite Development\nWeb app development,\nEmail hosting etc... \nCall/Whatsapp 0775231426\nOr visit our offices at  *VENTA TECH LOCATION*: SHOP NO1 Above Pick N Pay Shops (Shops above TM *Entrance between TM and Post*), Hwange ",
-  ];
-  
-  module.exports = advertMessages;
-  
+  "*Upgrade your storage game with SSDs!*\n Faster, quieter and more efficient than hard drives.\n With less moving parts they are also durable and resistant to shock or vibration making them perfect for laptops on the go.\nPlus, encryption means secure data protection. They are starting from just $35-00 at our shop located upstairs from TM shops.\n Call/whatsapp 0775231426 now for an upgrade you won't regret!",
+  "Looking for high-quality printing supplies? Look no further than Venta Tech! Find everything you need, including toner powder and refill ink to keep your printer running smoothly. Plus, say goodbye to costly replacements with our toner cartridge reset chips - they'll have you saving money in no time. And don't forget about the essential imaging drum and magnetic drum* that provide stunning print quality every time.\nVisit us at Shop1 TM mall today (located upstairs from TM)! Just look for our entrance between Post office and TM behind new Banc ABC. #PrintingSupplies #VentaTech ",
+  "*$6 32GB $8 64GB* Original Kingston data traveller flash drive \n Call/Whatsapp 0775231426/0812821641",
+  "Toshiba, Pantum ,Xerox, Ricoh, Bizhub? Looking for top-quality printer consumables that won't let you down? Look no further than Venta tech - your one-stop-shop for all things printing. From heavy-duty toner cartridges to ink refills, we've got everything you need to keep your printers running smoothly.We're proud suppliers of some of the most trusted brands in the industry, such as Ricoh, Bizhub, Xeroxnd Toshib And with our wide range of products up for grabs - including refill toner powder and copier master & ink supplies - there`s truly something here for everyone.\nSo why wait? Give us a call or shoot us a message on WhatsApp at 0775231426 today! You'll find our shop conveniently located at fisrt shop at Pick n Pay Mall (upstairs from TM; entrance between TM and Post Office)",
+  "What is your excuse? Still using a Hard Disk Drive (HDD) In 2023??\nAre your files not worth anything to you??\nSSDs are cheaper 3x faster and more durable.Call us and get yours *starting from $35* (including free intallaton of OS and migration of files)",
+  "*Toner powder,\nRefill ink,\nToner cartridge reset chip,\nImaging drum,\nMagnetic Drum*\nAll available at Venta Tech, Shop1 TM mall( Shops upstairs from TM), entrance is between between Post office and TM (behind new Banc ABC)",
+  "Grab an Original Kingston data traveler flash drive for $6 (32GB) or $8 (64GB) from Shop 1 at TM Shops upstairs. Look for the entrance between POST and Pick n Pay! Contact us now on call/WhatsApp: 0775231426/0812821641 to place your order",
+  "*Toner cartridges,Ink Cartridges, Master and ink for heavy duty copiers* , \n Venta tech are the premier suppliers of all printer consumables. *Contact us*  for all major brands of printers, HP , Kyocera, Nashua, Lexmark, Canon , Samsung i.e. *Also available* refill toner powder,cartridges, master & ink for copiers are available. Call or whatsapp *0775231426* or visit our shop at TM Pick n Pay Mall ( Shops above TM *Entrance between TM and Post office*) ",
+  " Original printer consumables available at great prices. Visit Millennium Printers, Shop 1 Baobab Service Station. In stock Hp ,Kyocera,Nashua, Ricoh,Samsung and all other major brands, Call us or whatsapp on 0775 231 426 for enquiries,or visit our shop at Total Baobab Service Station",
+  `*Need reliable CCTV camera installation* services to boost your peace of mind? Look no further than Venta Tech. We offer state-of-the-art  surveillance cameras with remote monitoring features and facial recognition technology. Stay safe 24/7 with infrared light capabilities, motion detection alerts, and live feeds accessible from your phone or computer. *Contact us at 0775 231 426 or send a WhatsApp message today!* `,
+  `*Computer Repairs, Printer Servicing* \n Reliable,Proffessional Computer hardware and Software Repairs by industry experts. \n Please inbox for enquiries`,
+  //"*FASTEST WIFI HOTSPOT IN THE LAND* \n Visit us at \n  VENTA TECH LOCATION: SHOP NO1 Above Pick N Pay Shops (Shops above TM *Entrance between TM and Post*), Hwange   APP or CALL0775231426 Download :\nMovies \n Music \nSoftwares \n   $1 Voucher For 1 hour \n $3 Voucher for 2GB  \n 30 minutes free WiFi for school children in school uniforms",
+
+  "*Serialised Receipt Books In full colour*, Business Documents,VENTA TECH provides proffessionally business documents such as Receipt books,Invoice Books, \n LOCATION SHOP NO. 1 Above TM Pick N Pay Flats Hwange \n App or Call 0775231426 \n For all your \n \n Receipts Books  \nInvoice Books \nLog Books \nReports Books \n 20 pack display books \nA4 Envelopes  \n Rulers \nMathematical sets \nDictionary \nSecondary text books ",
+  "*VSAT Internet*, your internet solution for remote areas.\nCall us on 0775231426 and you can have an internet connetcion within 48 hours,literally anywhere , we have unlimited coverage. Ideal for safaris, mines, rural setup etc.Fast reliable internet from Utande:  *Registered Utande reseller*.",
+  "*Automated WhatsApp chatbot* Run automatic adverts, automatic replies, bulk messaging, automatic alerts\nWebsite Development\nWeb app development,\nEmail hosting etc... \nCall/Whatsapp 0775231426\nOr visit our offices at  *VENTA TECH LOCATION*: SHOP NO1 Above Pick N Pay Shops (Shops above TM *Entrance between TM and Post*), Hwange ",
+];
+
+module.exports = advertMessages;
