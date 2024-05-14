6a2d2e84c88693d5467d5ace1e40ef59ed867427
mackawara <mkawara@outlook.com>
2024-05-14 16:57:08 +0200

    fix: add web config

--- config/wwebjsConfig.js
+++ config/wwebjsConfig.js
@@ -1,30 +1,52 @@
-const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+console.log(process.env.NODE_ENV);
+const wwebVersion='2.2412.54'
 const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        executablePath:
-          process.env.NODE_ENV == 'staging' ? null : process.env.EXECPATH,
-        handleSIGINT: true,
-        ignoreDefaultArgs: ['--enable-automation'],
-        headless: true, //process.env.NODE_ENV == 'local' ? false : true,
-        args: [
-          '--log-level=3', // fatal only
-          '--start-maximized',
-          '--no-default-browser-check',
-          '--disable-infobars',
-          '--disable-web-security',
-          '--disable-site-isolation-trials',
-          '--no-experiments',
-          '--ignore-gpu-blacklist',
-          '--ignore-certificate-errors',
-          '--ignore-certificate-errors-spki-list',
-          '--disable-gpu',
-          '--disable-extensions',
-          '--disable-default-apps',
-          '--enable-features=NetworkService',
-          '--disable-setuid-sandbox',
-          '--no-sandbox',
-        ],
-      },
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    executablePath:
+      process.env.NODE_ENV == 'local' ? null : process.env.EXECPATH,
+    handleSIGINT: true,
+    //ignoreDefaultArgs: ['--enable-automation'],
+    ignoreDefaultArgs: ['--disable-dev-shm-usage'], ignoreHTTPSErrors: true ,
+    headless: process.env.NODE_ENV == 'local' ? false : true,
+    args: [
+      '--log-level=3', // fatal only
+      '--start-maximized',
+      '--no-default-browser-check',
+      '--disable-infobars',
+      '--disable-web-security',
+      '--disable-site-isolation-trials',
+      '--no-experiments',
+      '--ignore-gpu-blacklist',
+      '--ignore-certificate-errors',
+      '--ignore-certificate-errors-spki-list',
+      '--disable-gpu',
+      '--disable-extensions',
+      '--disable-default-apps',
+      '--enable-features=NetworkService',
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+    ],
+  },
+  webVersionCache: {
+    type: 'remote',
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,//https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.htm
+}, 
 });
+
 module.exports = { client, MessageMedia };
+
+const wwebVersion = '2.2412.54';
+
+const client = new Client({
+    authStrategy: new LocalAuth(), // your authstrategy here
+    puppeteer: {
+        // puppeteer args here
+    },
+    // locking the wweb version
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    },
+});
